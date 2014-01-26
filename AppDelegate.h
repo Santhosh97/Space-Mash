d0685a5ac2c1cddbf9b67115417ff4090397e44c
Santhosh97 <santhoshsubramanian101@gmail.com>
2014-01-26 17:38:23 -0500

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tech Fair
+//
+//  Created by Santhosh Subramanian on 1/24/14.
+//  Copyright (c) 2014 Santhosh Subramanian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
